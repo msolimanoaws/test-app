b1896bf52e6e3c4a94f3a0d517679c83a39b99f7
MsolimanoM <marco.solimanomiranda@amaris.com>
2021-09-05 11:46:52 -0500

    feat/msolimano

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/msolimano.js</code> and save to reload.
+          Edit <code>src/domingo.js</code> and save to reload.
         </p>
         <a
           className="App-link"
